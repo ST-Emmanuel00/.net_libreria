c64485dbe59886aae98c0516d64150a520fda94f
Emmanuel Tabares Ortiz <emanuel_tabares@hotmail.com>
2023-08-19 12:39:52 -0500

    Se actualizo los mensajes de notificaciones y funciones de editar, eliminar

--- Controllers/LibroController.cs
+++ Controllers/LibroController.cs
@@ -1,6 +1,8 @@
 ﻿using Libreria.Data;
 using Libreria.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Libreria.Controllers
 {
@@ -8,19 +10,29 @@ namespace Libreria.Controllers
     {
 
         private readonly AplicationDbContext _context;
-        
+
 
         public LibroController(AplicationDbContext context)
         {
 
             _context = context;
 
-           
+
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index(String buscar)
         {
-            IEnumerable<Libro> ListaLibros = _context.libro;
-            return View(ListaLibros);
+            var libro = from Libro in _context.libro select Libro;
+
+            if (!String.IsNullOrEmpty(buscar))
+            {
+
+                libro = libro.Where(s => s.titulo!.Contains(buscar));
+
+            }
+
+
+
+            return View(await libro.ToListAsync());
         }
 
         public IActionResult CrearLibro()
@@ -36,11 +48,98 @@ namespace Libreria.Controllers
             {
                 _context.libro.Add(libro);
                 _context.SaveChanges();
+
+                TempData["mensaje"] = $"Se agrego {libro.titulo} exitosamente";
+
                 return RedirectToAction("Index");
 
             }
 
             return View();
+        }
+
+        public IActionResult EditarLibro(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound(); //Cuando no encuentra la pagina o error 404
+            }
+
+            var libro = _context.libro.Find(id);
+
+            if (libro == null)
+
+            {
+                return NotFound(); //Cuando no encuentra la pagina o error 404
+            }
+
+            return View(libro); //Para que muestre el formulario
+        }
+
+        [HttpPost]
+
+        public IActionResult EditarLibro(Libro libro)
+        {
+            //Validar el modelo libro
+            if (ModelState.IsValid) //Condición para validar el modelo
+            {
+                _context.libro.Update(libro); //Si el modelo es valido para agregar un libro
+                _context.SaveChanges(); //Se guardan los cambios
+
+                TempData["mensaje"] = $"Se modifico {libro.titulo} exitosamente";
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        public IActionResult EliminarLibro(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound(); //Cuando no encuentra la pagina o error 404
+            }
+
+            var libro = _context.libro.Find(id);
+
+            if (libro == null)
+
+            {
+                return NotFound(); //Cuando no encuentra la pagina o error 404
+            }
+
+            TempData["Mensaje"] = $"Vas a eliminar {libro.titulo} escrito por {libro.autor}";
+
+
+            return View(libro);
+
+
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.libro == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.libro'  is null.");
+            }
+            var libro = await _context.libro.FindAsync(id);
+            if (libro != null)
+            {
+                _context.libro.Remove(libro);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LibroExists(int id)
+        {
+            return (_context.libro?.Any(e => e.id == id)).GetValueOrDefault();
         }
 
     }
--- Models/Libro.cs
+++ Models/Libro.cs
@@ -8,13 +8,13 @@ namespace Libreria.Models
         public int id { get; set; }
 
         [Required(ErrorMessage = "El titulo de la obra es obligatorio")]
-        [RegularExpression(@"^[a-zA-Z0-9]*$", ErrorMessage = "Teienes sibolos no permitidos")]
+        [RegularExpression(@"^[a-zA-Z0-9\s]*$", ErrorMessage = "Teienes sibolos no permitidos")]
         public string titulo { get; set; }
 
 
         [StringLength(200, ErrorMessage = "La descripcion no debe ser tan extensa, maximo 200 caracteres")]
         [Required(ErrorMessage = "La descripcion de la obra es obligatoria")]
-        [RegularExpression(@"^[a-zA-Z0-9]*$", ErrorMessage = "Tienes sibolos no permitidos")]
+        [RegularExpression(@"^[a-zA-Z0-9\s]*$", ErrorMessage = "Tienes sibolos no permitidos")]
         public string descripcion { get; set; }
 
 
@@ -25,7 +25,7 @@ namespace Libreria.Models
 
 
         [Required(ErrorMessage = "El autor es obligatorio")]
-        [RegularExpression(@"^[a-zA-Z]*$", ErrorMessage = "No recibimos numeros" )]
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "No recibimos numeros" )]
         public string autor { get; set; }
 
         [Range(0.01, 999999.99, ErrorMessage = "El precio debe estar entre {1} y {2}")]
